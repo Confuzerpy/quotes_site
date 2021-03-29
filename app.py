7d8b2dadccd658921d0ed388187db9621008f93e
Neo <CEOpm@protonmail.com>
2021-03-29 22:07:12 +0300

    Сделал css разметку и переделал app

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import sqlite3
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Coins, Base
 
 
 app = Flask(__name__)
 
-"""Получаем данные из БД"""
-def get_db_connection():
-    conn = sqlite3.connect('coins.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+engine = create_engine('sqlite:///coins.db')
+Base.metadata.bind = engine
 
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 """Главная страница"""
+
+
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    main = conn.execute('SELECT * FROM coins').fetchall()
-    conn.close()
+    main = session.query(Coins).all()
     return render_template('index.html', main=main)
